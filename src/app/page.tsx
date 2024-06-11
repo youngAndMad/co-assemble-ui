ca2e92a9cc63674af5f31b68bb9172d4247daf35
youngandmad <kkraken2005@gmail.com>
2024-06-11 10:43:57 +0600

    next ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 "use client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
-
   return <></>;
 }
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 import { ReactNode } from "react";
 import ClientProvider from "./ClientProvider";
+import { NextUIProvider } from "@nextui-org/react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ClientProvider>{children}</ClientProvider>;
+  return (
+    <ClientProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </ClientProvider>
+  );
 }
