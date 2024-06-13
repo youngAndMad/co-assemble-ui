ce43930b9f8e81f8953961e97947ed1bfb5f70ad
youngandmad <kkraken2005@gmail.com>
2024-06-13 17:51:30 +0600

    verify email page

--- src/app/auth/verify-email/page.tsx
+++ src/app/auth/verify-email/page.tsx
@@ -1,21 +1,60 @@
 "use client";
-import { useRouter } from "next/router";
+
+import apiClient from "@/libs/axios";
+import { sleep } from "@/libs/rxjs";
+import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+type EmailVerificationData = {
+  email: string;
+  token: string;
+};
+
+const extractQueryParams = (): EmailVerificationData => {
+  const params = new URLSearchParams(window.location.search);
+  console.log(params);
+  return { email: params.get("email")!, token: params.get("token")! };
+};
+
+const verifyEmailQuery = ({ email, token }: EmailVerificationData) => {
+  return useQuery({
+    queryKey: ["verify-email", { email, token }],
+    queryFn: async () => {
+      apiClient.get(`/api/v1/auth/verify-email`, {
+        params: { email, token },
+      });
+    },
+    refetchOnReconnect: true,
+  });
+};
 
 const VerifyEmail = () => {
+  const { email, token } = extractQueryParams();
+  if (!email || !token) {
+    return <h1>Invalid URL</h1>;
+  }
+
+  const verifyEmail = verifyEmailQuery({ email, token });
   const router = useRouter();
-  const { email, token } = router.query;
 
-  // Log the email and token to the console
-  console.log("Email:", email);
-  console.log("Token:", token);
+  if (verifyEmail.isSuccess) {
+    sleep(3000).then(async () => {
+      router.push("/some-main-page-shown-after-email-verification");
+    });
+  }
 
-  // Render your component based on the email and token
   return (
-    <div>
-      <h1>Verify Email</h1>
-      <p>Email: {email}</p>
-      <p>Token: {token}</p>
-    </div>
+    <>
+      {verifyEmail.isPending ? (
+        <h1>Verifying...</h1>
+      ) : verifyEmail.isError ? (
+        <>{verifyEmail.error.message}</>
+      ) : (
+        <div>
+          <h1>Email verified</h1>
+        </div>
+      )}
+    </>
   );
 };
 
