09d84f5bc2a7d25ddd9950f294a9ba290c79bf3e
youngandmad <kkraken2005@gmail.com>
2024-06-18 21:10:35 +0600

    modified dockerfile and nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,49 @@
+# FROM node:lts as dependencies
+# WORKDIR /co-assemble-ui
+# COPY package.json package-lock.json ./
+# RUN npm install --frozen-lockfile
+
+
+# FROM node:lts as builder
+# WORKDIR /co-assemble-ui
+# COPY . .
+# COPY --from=dependencies /co-assemble-ui/node_modules ./node_modules
+# RUN npm run build
+
+
+# FROM node:lts as runner
+# WORKDIR /co-assemble-ui
+# ENV NODE_ENV production
+
+# COPY --from=builder /co-assemble-ui/public ./public
+# COPY --from=builder /co-assemble-ui/package.json ./package.json
+# COPY --from=builder /co-assemble-ui/.next ./.next
+# COPY --from=builder /co-assemble-ui/node_modules ./node_modules
+
+# COPY nginx-default.conf /etc/nginx/conf.d/default.conf
+
+# EXPOSE 3000
+# CMD ["npm", "run" , "start"]
+
+# Stage 1: Dependencies
 FROM node:lts as dependencies
 WORKDIR /co-assemble-ui
 COPY package.json package-lock.json ./
 RUN npm install --frozen-lockfile
 
-
+# Stage 2: Build
 FROM node:lts as builder
 WORKDIR /co-assemble-ui
 COPY . .
 COPY --from=dependencies /co-assemble-ui/node_modules ./node_modules
 RUN npm run build
 
-
-FROM node:lts as runner
-WORKDIR /co-assemble-ui
-ENV NODE_ENV production
-
-COPY --from=builder /co-assemble-ui/public ./public
-COPY --from=builder /co-assemble-ui/package.json ./package.json
-COPY --from=builder /co-assemble-ui/.next ./.next
-COPY --from=builder /co-assemble-ui/node_modules ./node_modules
-
+# Stage 3: Runner
+FROM nginx:alpine as runner
+COPY --from=builder /co-assemble-ui/.next /usr/share/nginx/html/.next
+COPY --from=builder /co-assemble-ui/public /usr/share/nginx/html/public
+COPY --from=builder /co-assemble-ui/package.json /usr/share/nginx/html/package.json
 COPY nginx-default.conf /etc/nginx/conf.d/default.conf
 
-EXPOSE 3000
-CMD ["npm", "run" , "start"]
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,61 +1,61 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import {Providers} from "@/providers/Providers";
-import {ToastContainer} from "react-toastify";
+import { Providers } from "@/providers/Providers";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import {cn} from "@/libs/utils";
+import { cn } from "@/libs/utils";
 import React from "react";
 
 const inter = Inter({
-    subsets: ["latin"],
-    variable: "--font-sans"
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-    title: "Coasseble",
-    authors: {
-        name: "Danekerscode",
-        url: "https://github.com/youngAndMad",
-    },
+  title: "Coasseble",
+  authors: {
+    name: "Danekerscode",
+    url: "https://github.com/youngAndMad",
+  },
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-        <head>
-            <link
-                href="https://fonts.googleapis.com/css?family=Montserrat:300, 400, 500"
-                rel="stylesheet"
-            />
-            <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
-            />
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link
-                rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
-            />
+  return (
+    <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:300, 400, 500"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
 
-            <title>
-                <>
-                    {metadata.title}
-                </>
-            </title>
-        </head>
-        <body className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            inter.variable
-        )}>
+        <title>
+          <>{metadata.title}</>
+        </title>
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
         <Providers>{children}</Providers>
-        <ToastContainer/>
-        </body>
-        </html>
-    );
+        <ToastContainer />
+      </body>
+    </html>
+  );
 }
